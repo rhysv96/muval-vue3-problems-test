5411db417b5960548aa7db7880938d75c8dd3b2d
Rhys Van Houten <rhys.vanhouten@gmail.com>
2025-02-25 18:01:12 +1000

    add pinia

--- src/stores/counter.ts
+++ src/stores/counter.ts
@@ -1,2 +1,23 @@
-// replace below with a store. the below only exists to silence errors
-export const useCounterStore = () => ({ increment: null })
+import { defineStore } from "pinia";
+import { ref } from "vue";
+
+// option style, if you like that
+// eslint-disable-next-line
+const useCounterStoreOptionStyle = defineStore('counter-option', {
+  state: () => ({
+    count: 0,
+  }),
+  actions: {
+    increment() {
+      this.count++
+    },
+  },
+})
+
+// composition style, as well. I think I prefer this one so that's what I'll use
+export const useCounterStore = defineStore('counter-composition', () => {
+  const count = ref(0)
+  const increment = () => count.value++
+
+  return { count, increment }
+})
